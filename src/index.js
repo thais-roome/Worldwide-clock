9d945d5fd85eef0cd5d94ffc46acc6c6d3f7e5f2
thais-roome <tee.szimanski@gmail.com>
2024-01-10 17:12:10 +0000

    Added link to all cities

--- src/index.js
+++ src/index.js
@@ -41,7 +41,8 @@ function updateCity(event) {
           </div>
           <div class="time"> ${cityTime.format("h:mm:ss")}
           <small>${cityTime.format(" A")}</small></div>
-          </div>`;
+          </div>
+          <a href="/"> All cities</a>`;
 }
 
 updateTime();
