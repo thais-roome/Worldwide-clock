f273c3d43a38186d8939031680a41b4e7306f6a4
thais-roome <tee.szimanski@gmail.com>
2024-01-10 16:58:35 +0000

    Added current time javascript

--- src/index.js
+++ src/index.js
@@ -28,20 +28,23 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
+  citiesElement.innerHTML = `<div class="city">
   <div>
   <h2>${cityName}</h2>
             <div class="date"> ${cityTime.format("MMMM Do YYYY")}</div>
           </div>
           <div class="time"> ${cityTime.format("h:mm:ss")}
-          <small>${cityTime.format("A")}</small>
+          <small>${cityTime.format(" A")}</small></div>
           </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
-let citiesSelect = document.querySelector("#city");
-citiesSelect.addEventListener("change", updateCity);
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
